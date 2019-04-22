5d08ab42eb483783a3a760bacb2469964694681e
Six-S <brenlink@gmail.com>
2019-04-22 00:50:59 -0600

    styling with JSX

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import ReactDOM from 'react-dom';
 
 // Create a React component
 const App = () => {
-    return <div>Hi There!</div>
+    return (
+        <div>
+            <label class='label' for='name'>
+                Enter Name:
+            </label>
+            <input id='name' type='text' />
+            <button style={{ backgroundColor: 'blue', color:'white' }}>
+                Submit
+            </button>
+        </div>
+    )
 };
 
 // Take the React Component and show it on the screen
